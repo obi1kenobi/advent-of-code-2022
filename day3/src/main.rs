ed6f8cb0a6ca7cbcd6294917e3f4df2e6a40e95a
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-12-03 18:34:56 +0000

    Solve day3.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,16 @@
-use std::{env, fs};
+use std::{collections::BTreeSet, env, fs};
 
 #[allow(unused_imports)]
 use itertools::Itertools;
+
+fn priority(c: char) -> i64 {
+    let value = if c.is_ascii_lowercase() {
+        (c as u32) - ('a' as u32) + 1
+    } else {
+        (c as u32) - ('A' as u32) + 27
+    };
+    value as i64
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,11 +24,7 @@ fn main() {
     let input_file = reversed_args.pop().expect("input file");
     let content = fs::read_to_string(input_file).unwrap();
 
-    let input_data: Vec<u64> = content
-        .trim_end()
-        .split('\n')
-        .map(|x| x.parse().unwrap())
-        .collect();
+    let input_data: Vec<&str> = content.trim_end().split('\n').collect();
 
     match part {
         "1" => {
@@ -34,10 +39,41 @@ fn main() {
     }
 }
 
-fn solve_part1(_data: &[u64]) -> usize {
-    todo!()
+fn solve_part1(data: &[&str]) -> i64 {
+    data.iter()
+        .map(|rucksack| {
+            let (front, back) = rucksack.split_at(rucksack.len() / 2);
+            let front_chars: BTreeSet<_> = front.chars().collect();
+
+            for back_char in back.chars() {
+                if front_chars.contains(&back_char) {
+                    return priority(back_char);
+                }
+            }
+            unreachable!()
+        })
+        .sum()
 }
 
-fn solve_part2(_data: &[u64]) -> usize {
-    todo!()
+fn solve_part2(data: &[&str]) -> i64 {
+    data.iter()
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| {
+            let (first, second, third) = chunk.copied().collect_tuple().unwrap();
+
+            let first_chars = first.chars().collect::<BTreeSet<_>>();
+            let second_chars = second.chars().collect::<BTreeSet<_>>();
+            let third_chars = third.chars().collect::<BTreeSet<_>>();
+
+            let first_intersect = first_chars
+                .intersection(&second_chars)
+                .copied()
+                .collect::<BTreeSet<_>>();
+
+            let common_item = first_intersect.intersection(&third_chars).next().unwrap();
+
+            priority(*common_item)
+        })
+        .sum()
 }
