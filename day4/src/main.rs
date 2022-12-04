faf9a529e0268c94ecfd372aae17c1c1e41dd0c3
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-12-04 18:25:33 +0000

    Solve day4.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, ops::RangeInclusive};
 
 #[allow(unused_imports)]
 use itertools::Itertools;
+
+fn parse_range(range: &str) -> RangeInclusive<i64> {
+    let (from, to) = range.split_once('-').unwrap();
+    from.parse().unwrap()..=to.parse().unwrap()
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,10 +20,13 @@ fn main() {
     let input_file = reversed_args.pop().expect("input file");
     let content = fs::read_to_string(input_file).unwrap();
 
-    let input_data: Vec<u64> = content
+    let input_data: Vec<(RangeInclusive<i64>, RangeInclusive<i64>)> = content
         .trim_end()
         .split('\n')
-        .map(|x| x.parse().unwrap())
+        .map(|x| {
+            let (left, right) = x.split_once(',').unwrap();
+            (parse_range(left), parse_range(right))
+        })
         .collect();
 
     match part {
@@ -34,10 +42,25 @@ fn main() {
     }
 }
 
-fn solve_part1(_data: &[u64]) -> usize {
-    todo!()
+fn fully_contains(bigger: &RangeInclusive<i64>, smaller: &RangeInclusive<i64>) -> bool {
+    bigger.start() <= smaller.start() && bigger.end() >= smaller.end()
 }
 
-fn solve_part2(_data: &[u64]) -> usize {
-    todo!()
+fn intersect(left: &RangeInclusive<i64>, right: &RangeInclusive<i64>) -> bool {
+    let larger_start = std::cmp::max(left.start(), right.start());
+    let smaller_end = std::cmp::min(left.end(), right.end());
+
+    larger_start <= smaller_end
+}
+
+fn solve_part1(data: &[(RangeInclusive<i64>, RangeInclusive<i64>)]) -> usize {
+    data.iter()
+        .filter(|(left, right)| fully_contains(left, right) || fully_contains(right, left))
+        .count()
+}
+
+fn solve_part2(data: &[(RangeInclusive<i64>, RangeInclusive<i64>)]) -> usize {
+    data.iter()
+        .filter(|(left, right)| intersect(left, right))
+        .count()
 }
