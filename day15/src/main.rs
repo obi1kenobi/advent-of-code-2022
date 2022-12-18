f73116c539a53c86595ce55a5af16fa258ae6a36
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-12-18 13:24:05 +0000

    Add parsing code for day15.

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,7 +1,102 @@
-use std::{env, fs};
+use std::{
+    collections::BTreeSet,
+    env, fs,
+    ops::{Add, Neg, Sub},
+};
 
 #[allow(unused_imports)]
 use itertools::Itertools;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[repr(u8)]
+enum Direction {
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3,
+}
+
+impl Direction {
+    const ALL_UNIT_VECTORS: [(i64, i64); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+    fn unit_vector(&self) -> Vector2D {
+        // SAFETY: Because `Self` is marked `repr(u8)`, its layout is a `repr(C)` `union`
+        // between `repr(C)` structs, each of which has the `u8` discriminant as its first
+        // field, so we can read the discriminant without offsetting the pointer.
+        let idx = unsafe { *<*const _>::from(self).cast::<u8>() } as usize;
+        let (x, y) = Direction::ALL_UNIT_VECTORS[idx];
+        Vector2D::new(x, y)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Vector2D {
+    x: i64,
+    y: i64,
+}
+
+impl Vector2D {
+    fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+
+    fn zero() -> Self {
+        Self { x: 0, y: 0 }
+    }
+}
+
+impl Add<Vector2D> for Vector2D {
+    type Output = Vector2D;
+
+    fn add(self, rhs: Vector2D) -> Self::Output {
+        Vector2D {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Neg for Vector2D {
+    type Output = Vector2D;
+
+    fn neg(self) -> Self::Output {
+        Vector2D {
+            x: -self.x,
+            y: -self.y,
+        }
+    }
+}
+
+impl Sub<Vector2D> for Vector2D {
+    type Output = Vector2D;
+
+    fn sub(self, rhs: Vector2D) -> Self::Output {
+        self + (-rhs)
+    }
+}
+
+fn parse_point(point: &str) -> Vector2D {
+    point
+        .strip_prefix("x=")
+        .expect("point doesn't start with 'x='")
+        .split_once(", y=")
+        .map(|(x, y)| {
+            let x = x.parse().unwrap();
+            let y = y.parse().unwrap();
+            Vector2D::new(x, y)
+        })
+        .expect("point coordinates not delimited correctly")
+}
+
+fn parse_line(line: &str) -> (Vector2D, Vector2D) {
+    line
+        .strip_prefix("Sensor at ")
+        .expect("bad line start")
+        .trim_end()
+        .split_once(": closest beacon is at ")
+        .map(|(sensor, beacon)| (parse_point(sensor), parse_point(beacon)))
+        .expect("split failed")
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,10 +110,10 @@ fn main() {
     let input_file = reversed_args.pop().expect("input file");
     let content = fs::read_to_string(input_file).unwrap();
 
-    let input_data: Vec<u64> = content
+    let input_data: Vec<(Vector2D, Vector2D)> = content
         .trim_end()
         .split('\n')
-        .map(|x| x.parse().unwrap())
+        .map(parse_line)
         .collect();
 
     match part {
@@ -34,10 +129,10 @@ fn main() {
     }
 }
 
-fn solve_part1(_data: &[u64]) -> usize {
+fn solve_part1(data: &[(Vector2D, Vector2D)]) -> usize {
     todo!()
 }
 
-fn solve_part2(_data: &[u64]) -> usize {
+fn solve_part2(data: &[(Vector2D, Vector2D)]) -> usize {
     todo!()
 }
